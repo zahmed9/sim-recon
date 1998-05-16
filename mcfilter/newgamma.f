5e62f7f277a892d0ef98a0b3b91ac49d8f6b51d6
Curtis Meyer <cmeyer@jlab.org>
1998-05-16 16:20:30 +0000

    Moved the resolution control to det_resol.h.

--- mcfilter/newgamma.f
+++ mcfilter/newgamma.f
@@ -1,31 +1,60 @@
 *
       subroutine newgamma(p)
-c
-c     takes a photon 4-vector (stored in p)
-c     and smears it accoding to the resolution function:
 *
-*        Modified: 14 May 1998 -- P(px,py,pz,E)
-c
-c     sigma     stat
-c     ----- =  ------   + floor
-c       E      sqrt(E)
-c
-CCCC      include 'dev.inc'
+*        takes a photon 4-vector (stored in p)
+*        and smears it accoding to the resolution function:
+*
+*           sigma    Gam_stat
+*           ----- =  --------   + Gam_floor
+*             E       sqrt(E)
+*
+*        This routine should also smear the photon direction in 
+*        addition to the photon energy. 
+*
+*     __________________________________________________________
+*
+      Implicit None
+*
       real p(4)
 *
-      stat  = 0.05
-      floor = 0.02
+*        sigma: Gaussian width for the energy.
+*        eold:  Original photon energy.
+*        enew:  Smeared  photon energy.
 *
-      sigma=p(4)*floor + sqrt(p(4))*stat
-cvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-      sigma=0.
-c^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- 1    continue
-      eold=p(4)
-      enew=p(4)+sigma*gausran(dummy)
-      if(enew.le.0.) go to 1
-      do i=1,4
-         p(i)=p(i)*enew/eold
-      enddo
+      real  sigma
+      real  eold
+      real  enew
+*
+      integer i
+*
+*        gausran: Funcion to produce a gaussian distributed random
+*                 number.
+*
+      real  gausran
+      real  dummy
+      external gausran
+*
+      include "det_resol.h"
+*
+*     __________________________________________________
+*
+*        Compute the gaussian width for the photon energy.
+*
+      sigma = p(4) * Gam_floor + sqrt(p(4)) * Gam_stat
+*
+      eold = p(4)
+*
+*        Compute the smeared photon energy, but do not allow the
+*        energy to become negative.
+*
+ 100  enew = p(4) + sigma * gausran(dummy)
+      if ( enew .le. 0.0 ) go to 100
+*
+*        Smear the photon four-vector.
+*
+      do 200 i = 1,4
+         p(i) = p(i) * (enew / eold)
+ 200  continue
+*
       return
       end
