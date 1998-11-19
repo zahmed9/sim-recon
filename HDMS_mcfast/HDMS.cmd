89f333619de3ba32f92203cde9d4ae24c2851117
Paul Eugenio <eugenio@jlab.org>
1998-11-19 00:16:03 +0000

    Modified to use run_mcfast

--- HDMS_mcfast/HDMS.cmd
+++ HDMS_mcfast/HDMS.cmd
@@ -1,10 +1,10 @@
 !
-! Cebaf Meson Spectrometer command file for MCFast.
+! Cebaf HallD Meson Spectrometer command file for MCFast.
 !
 max_ev    25000                    !Max number of events
 max_print 4                      !max # events to print
-!file_geo $CMS_MCFAST_DIR/CMS_v_1.0.db       !Geometry file
-file_geo /home/halld/source/CMS_mcfast/CMS_v_1.0.db       !Geometry file
+!file_geo $HDMS_MCFAST_DIR/HDMS.db       !Geometry file
+file_geo /home/halld/source/HDMS_mcfast/HDMS.db       !Geometry file
 ranseed  0                       !Random seeds =0 use time()
 file_type 1                      !MCFio input file; 1 = StdHep file.
 file_in input.evt                  !data files: enter either 
