6cbcb448727104913fe7086ab143e9b37cb5a1e9
Paul Eugenio <eugenio@jlab.org>
2004-06-10 15:31:13 +0000

    Added TOFMass methods

--- mcroot/TMCFastTraceEvent.cxx
+++ mcroot/TMCFastTraceEvent.cxx
@@ -122,8 +122,10 @@ double TMCFastTraceEvent::GetTZero(int partNo){
 }
 
 double TMCFastTraceEvent::GetMass(int partNo){
-  // Get the particle mass
+  // Get the real particle mass
   //   m = sqrt(E^2 - P^2)
+  //
+  // This method uses trace point info.
 
   TClonesArray *tpoints =this->GetTracePoints();
   TClonesArray *tparts =this->GetTraceParticles();
@@ -137,65 +139,140 @@ double TMCFastTraceEvent::GetMass(int partNo){
 }
 
 
+
+double TMCFastTraceEvent::GetTOFMass(int partNo, int direction, double location){
+ // Calculate time-of-flight mass
+  // Currently there is no smearing!!!!! (but soon should be implemented)
+  //
+  //  m = p/(beta*gamma)
+  //
+  //  beta = (tracepoint->GetPath(i)/tof)*(1/kSpeedOfLight);
+  //  tof = GetCTOF(partNo, R);
+  //
+  // return -1 if GetCTOF(partNo, R) fails to find a tracepoint.
+  // Note: the value of direction can equal "kFORWARD" or "kCENTRAL" 
+
+ double mass = -1; // default return  if GetCTOF(partNo, R) fails to find a tracepoint.
+
+ TMCFastTracePoint *tracepoint = GetTracePoint(partNo, direction, location); 
+ double tof = GetTOF(partNo, direction, location);
+ double path = tracepoint->GetPath();
+
+ if(tof>0){
+   double beta = (path/tof)*(1/kSpeedOfLight);
+   double gamma = 1/(TMath::Sqrt( 1 - beta*beta));
+
+   if(gamma!=gamma) // look for NaN this means beta=1
+     mass =0;
+   else
+     mass =   tracepoint->GetP()/(beta*gamma);
+ }
+ return  mass;
+}
+
+double TMCFastTraceEvent::GetCTOFMass(int partNo, double R){
+  // Calculate time-of-flight mass
+  //
+  // Uses GetTOFMass(partNo, kCENTRAL, R);
+  
+  return this->GetTOFMass(partNo, kCENTRAL, R);
+}
+
+double TMCFastTraceEvent::GetFTOFMass(int partNo, double Z){
+  // Calculate time-of-flight mass
+  //
+  // Uses GetTOFMass(partNo, kFORWARD, Z);
+  
+  return this->GetTOFMass(partNo, kFORWARD, Z);
+}
+
+double TMCFastTraceEvent::GetTOF(int partNo,int direction, double location){
+  // Get TOF from production to either forward or central location
+  //
+  // Function returns a negative number if
+  // particle never reached Z location
+  //
+  // Note: the value of direction can equal "kFORWARD" or "kCENTRAL" 
+  //
+  double tof = GetT(partNo,direction,location) -  GetTZero(partNo);
+  return  tof>0? tof:-1;
+}
+
+
+
 double TMCFastTraceEvent::GetFTOF(int partNo, double Z){
   // Get Forward TOF from production to Z location
   //
   // Function returns a negative number if
   // particle never reached Z location
-  double tof = GetFT(partNo,Z) -  GetTZero(partNo);
-  return  tof>0? tof:-1;
+ 
+  return  GetTOF(partNo,kFORWARD, Z);
+  
 }
 
 double TMCFastTraceEvent::GetCTOF(int partNo, double R){
   // Get Central TOF from production to radius R
   //
   // Function returns a negative number if
-  // particle never reached Z location
-   double tof =  GetCT(partNo,R) -  GetTZero(partNo);
-  return tof>0? tof:-1;
+  // particle never reached R location
+  
+  GetTOF(partNo,kCENTRAL, R);
 }
 
 
-double TMCFastTraceEvent::GetFT(int partNo, double Z){
+double TMCFastTraceEvent::GetT(int partNo, int direction, double location){
   // Get the mcfast clock time for
-  // a particle at Z location
+  // a particle moving in the forward/central direction at the given location
+  //
+  // Note: the value of direction can equal "kFORWARD" or "kCENTRAL" 
+  // Method return -1 if tracepoint at location is not found.
+ 
 
   double time=-1; //default
  
-  TMCFastTracePoint *tracepoint = GetTracePointZ(partNo, Z);
-  double zloc = tracepoint->GetZ();  
-  if(zloc>= Z) 
+  TMCFastTracePoint *tracepoint = GetTracePoint(partNo, direction,location);
+  double loc = direction==kFORWARD?tracepoint->GetZ():tracepoint->GetR();  
+  if(loc >= location ) //we found closest tracepoint
     time =  tracepoint->GetTime();
-  
   return time;
 }
 
-
-double TMCFastTraceEvent::GetCT(int partNo, double R){
+double TMCFastTraceEvent::GetFT(int partNo, double Z){
   // Get the mcfast clock time for
-  // a particle at radius R
+  // a particle at Forward location  [Uses GetT(partNo,direction,location)]
 
-  double time=-1; //default
+  return this->GetT(partNo,kFORWARD,Z);
 
-  TMCFastTracePoint *tracepoint = GetTracePointR(partNo, R);
-  double rloc = tracepoint->GetR();
-  if(rloc>= R){ 
-    time =  tracepoint->GetTime();
-  }
-  
-  return time;
+}
+
+double TMCFastTraceEvent::GetCT(int partNo, double Z){
+  // Get the mcfast clock time for
+  // a particle at the Central location  [Uses GetT(partNo,direction,location)]
+
+  return this->GetT(partNo,kCENTRAL,Z);
+
 }
 
 
+TMCFastTracePoint* TMCFastTraceEvent::GetTracePoint(int partNo){
+  // Get the particle's 1st trace point.  This is the trace point at
+  // the production vertex
 
+  TClonesArray *tpoints =this->GetTracePoints();
+  TClonesArray *tparts =this->GetTraceParticles();
+  TMCFastTracePoint *tracepoint=0;
+  TMCFastTraceParticle *tracepart;
 
+  tracepart =  (TMCFastTraceParticle *)(*tparts)[partNo];
+  tracepoint =  (TMCFastTracePoint *)(*tpoints)[tracepart->GetFirst()];
+  return  tracepoint;
+}
 
 TMCFastTracePoint* TMCFastTraceEvent::GetTracePoint(int partNo, int direction, double Position){
   // Get the particle trace point closest to the  location requested
-  // Direction=0 refers to forward/z tracing (i.e. uses  tracepoint->GetZ())
-  // whereas direction =1 refers to central/radial tracing (i.e. uses  tracepoint->GetR())
-  
-  if(direction != 0 && direction != 1){
+  // direction can equal "kFORWARD" or "kCENTRAL" which refers to forward/z tracing 
+  //
+  if(direction != kFORWARD && direction != kCENTRAL){
     // error: wrong direction
     cerr<<"WARNING:::: Using TMCFastTraceEvent::GetTracePoint() with invalid direction\n";
   }
@@ -215,52 +292,18 @@ TMCFastTracePoint* TMCFastTraceEvent::GetTracePoint(int partNo, int direction, d
   return  tracepoint;
 }
 
-
-
 TMCFastTracePoint* TMCFastTraceEvent::GetTracePointR(int partNo, double R){
   // Get the particle trace point closest to the Z location requested
   //  
     
-  /*
-  TClonesArray *tpoints =this->GetTracePoints();
-  TClonesArray *tparts =this->GetTraceParticles();
-  TMCFastTracePoint *tracepoint=0;
-  TMCFastTraceParticle *tracepart;
-  tracepart =  (TMCFastTraceParticle *)(*tparts)[partNo];
-
-  for(int i = tracepart->GetFirst(); i<=tracepart->GetLast();i++){
-   tracepoint =  (TMCFastTracePoint *)(*tpoints)[i];
-   double loc = tracepoint->GetR();
-   if(loc>= R)
-     i = tracepart->GetLast()+1; //get out of loop   
-  }
-  */
-  return  GetTracePoint(partNo, 1,R);
+  return  GetTracePoint(partNo,kCENTRAL,R);
 }
-
-
 
 TMCFastTracePoint* TMCFastTraceEvent::GetTracePointZ(int partNo, double Z){
   // Get the particle trace point closest to the Z location requested
   //  
- 
-  /*   
-  TClonesArray *tpoints =this->GetTracePoints();
-  TClonesArray *tparts =this->GetTraceParticles();
-  TMCFastTracePoint *tracepoint=0;
-  TMCFastTraceParticle *tracepart;
-  tracepart =  (TMCFastTraceParticle *)(*tparts)[partNo];
 
-  for(int i = tracepart->GetFirst(); i<=tracepart->GetLast();i++){
-   tracepoint =  (TMCFastTracePoint *)(*tpoints)[i];
-   double loc = tracepoint->GetZ();
-   if(loc>= Z)
-     i = tracepart->GetLast()+1; //get out of loop   
-  }
-
-  return  tracepoint;
-  */
-  return GetTracePoint(partNo, 0,Z);
+  return GetTracePoint(partNo, kFORWARD,Z);
 }
 
 
--- mcroot/TMCFastTraceEvent.h
+++ mcroot/TMCFastTraceEvent.h
@@ -21,6 +21,9 @@
 using namespace std;
 
 #define MAX_TRACE_PARTICLES 100
+const int kFORWARD=0; //Z direction
+const int kCENTRAL=1; //Radial direction
+const Double_t kSpeedOfLight=29.9792; // units: cm/nSec
 
 class TMCFastTraceEvent: public TObject {
   
@@ -49,15 +52,23 @@ public:
 
   int GetNTracePoints(int partNo) ;
   double GetTZero(int partNo);
+  double GetT(int partNo, int direction, double location);
   double GetFT(int partNo, double Z);
   double GetCT(int partNo, double R);
+  double GetTOF(int partNo, int direction, double location);
   double GetFTOF(int partNo, double Z);
   double GetCTOF(int partNo, double R);
+  double GetTOFMass(int partNo, int direction, double location);
+  double GetFTOFMass(int partNo, double Z);
+  double GetCTOFMass(int partNo, double R);
   double GetMass(int partNo);
+
  
   TMCFastTracePoint* GetTracePointZ(int partNo, double Z);
   TMCFastTracePoint* GetTracePointR(int partNo, double R);
   TMCFastTracePoint* GetTracePoint(int partNo, int direction, double Position);
+  TMCFastTracePoint* GetTracePoint(int partNo);
+
 
   void Print(ostream *os);
   ClassDef(TMCFastTraceEvent,1)  //Needed by ROOT
