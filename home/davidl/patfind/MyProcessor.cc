bd2d817e73a6f905103d597c149221ba578e7498
David Lawrence <davidl@jlab.org>
2005-02-07 11:50:05 +0000

    Major revision. Mainly, moved a few things into a separate class (DTrkHit) to aid it in:
    1. Sparsely fill density histogram. Instead of looping through every
    hit for every bin of the large 2D histo, only fill bins surrounding
    line-of-circle-centers lines.
    
    2. Recursively fill density histogram by excluding contributions from
    hits which have already contributed to a track.
    
    3. Added use of density_work histo so that the first density histo with
    all hits can be saved for displaying.
    
    Also, the size of the density histo was increased to 240 x 240 cm^2
    with one bin per square cm. This needs to be optimized.

--- home/davidl/patfind/MyProcessor.cc
+++ home/davidl/patfind/MyProcessor.cc
@@ -43,15 +43,21 @@ derror_t MyProcessor::init(void)
 	// Initialize Nlines Nellipse;
 	Nlines = 0;
 	Nellipse = 0;
+	Ntrkhits = 0;
 	
 	// set limits for plot. This represents the space where the center 
 	// of the circle can be. It can be (and often is) outside of the
 	// bounds of the solenoid.
-	cmax = 100.0; // in cm.
+	cmax = 120.0; // in cm.
 
 	axes = new TH2F("axes","",10,-cmax,cmax,10,-cmax,cmax);
 	
-	density = new TH2F("density","Density",200,-cmax,cmax,200,-cmax,cmax);	
+	density = new TH2F("density","Density",240,-cmax,cmax,240,-cmax,cmax);	
+
+	// We want to leave the actual density histo with the full
+	// map for drawing. Hence, we make a duplicate here for
+	// working with.
+	density_work = new TH2F(*density);
 
 	return NOERROR;
 }
@@ -72,7 +78,7 @@ derror_t MyProcessor::evnt(int eventnumber)
 	for(int i=0;i<Nlines;i++)delete lines[i];
 	Nlines = 0;
 
-	// Get MCCheatHits and loop over them, adding them to the DQuickFit object
+	// Get MCCheatHits and loop over them
 	DContainer *mccheathits = event_loop->Get("MCCheatHits");
 	MCCheatHit_t *mccheathit = (MCCheatHit_t*)mccheathits->first();
 	for(int i=0;i<mccheathits->nrows;i++, mccheathit++){
@@ -107,8 +113,89 @@ derror_t MyProcessor::evnt(int eventnumber)
 		lines[Nlines++]->Draw();
 	}
 	
+	// Draw circles at focus points
+	for(int i = 0;i<Nellipse;i++)ellipse[i]->Draw();
+
 	maincanvas->Update();
 	cout<<endl<<"Done"<<endl;
+	
+	return NOERROR;
+}
+
+//------------------------------------------------------------------
+// FindTracks
+//------------------------------------------------------------------
+derror_t MyProcessor::FindTracks(void)
+{
+	/// Find tracks by repeatedly filling the density
+	/// histogram and looking for the maximum. As each
+	/// maximum is found, a TEllipse is created centered
+	// on the maximum.
+
+	// Delete old ellipses (tracks)
+	for(int i=0;i<Nellipse;i++)delete ellipse[i];
+	Nellipse = 0;
+	
+	// Delete old DTrkHits
+	for(int i=0;i<Ntrkhits;i++)delete trkhit[i];
+	Ntrkhits = 0;
+	
+	// Create a new DTrkHit object for each MCCheatHit
+	DContainer *mccheathits = event_loop->Get("MCCheatHits");
+	MCCheatHit_t *mccheathit = (MCCheatHit_t*)mccheathits->first();
+	for(Ntrkhits=0;Ntrkhits<mccheathits->nrows;Ntrkhits++, mccheathit++){
+		float x = mccheathit->r*cos(mccheathit->phi);
+		float y = mccheathit->r*sin(mccheathit->phi);
+
+		trkhit[Ntrkhits] = new DTrkHit(x,y);
+		if(Ntrkhits>=299)break;
+	}
+	
+	do{
+		TH2F *tmp;
+		tmp = Nellipse<1 ? density:density_work;
+	
+		// Fill the density histogram
+		FillDensityHistogram(tmp);
+
+		// Find the coordinates of the maximum
+		int xbin, ybin, zbin;
+		tmp->GetMaximumBin(xbin,ybin,zbin);
+		float x = tmp->GetXaxis()->GetBinCenter(xbin);
+		float y = tmp->GetYaxis()->GetBinCenter(ybin);
+
+		// Flag all hits within masksize of the focus as used
+		int Nhits_this_track = 0;
+		int Nhits_not_used = 0;
+		float masksize = 5.0;
+		DTrkHit **t = trkhit;
+		for(int i=0;i<Ntrkhits;i++ ,t++){
+			if((*t)->used)continue;
+			float d = (*t)->DistToLine(x,y);
+			if(d <= masksize){
+				(*t)->used = 1;
+				Nhits_this_track++;
+			}else{
+				Nhits_not_used++;
+			}
+		}
+		
+		// If there are less than 4 hits on this track, assume that
+		// means it's not really a track and we've already found them
+		// all.
+		if(Nhits_this_track<4)break;
+		
+		// Record the location of the maximum by making a TEllipse we can
+		// draw there later.
+		ellipse[Nellipse] = new TEllipse(x,y,masksize);
+		ellipse[Nellipse]->SetLineColor(Nellipse<3 ? kRed:kBlue);
+		Nellipse++;
+		
+		// If less than 4 hits remain unused, we can stop looking now
+		if(Nhits_not_used<4)break;
+
+	}while(Nellipse<10);
+	cout<<__FILE__<<":"<<__LINE__<<" Found "<<Nellipse<<" tracks"<<endl;
 	
 	return NOERROR;
 }
@@ -118,107 +205,38 @@ derror_t MyProcessor::evnt(int eventnumber)
 //------------------------------------------------------------------
 derror_t MyProcessor::densityPlot(void)
 {
-	TAxis *yaxis = density->GetYaxis();
-	TAxis *xaxis = density->GetXaxis();
-	int NbinsY = yaxis->GetNbins();
-	int NbinsX = xaxis->GetNbins();
 	
-	// Delete old circles
-	for(int i=0;i<Nellipse;i++)delete ellipse[i];
-	Nellipse = 0;
+	// If tracks haven't been found yet, then find them
+	if(Nellipse==0)FindTracks();
 	
-	// Fill density plot (if needed)
-	if(density->Integral()==0.0){
-		for(int i=1;i<=NbinsY;i++){
-			float y = yaxis->GetBinCenter(i);
-			for(int j=1;j<=NbinsX;j++){
-				float x = xaxis->GetBinCenter(j);
-			
-				FillDensityBin(x,y);
-			}
-		}
-	}
-	
+	// Draw contour map
 	density->Draw("cont");
 
-	// Indentify peaks
-	TH2F *tmp = new TH2F(*density);
-	double max = tmp->GetMaximum();
-	for(int Nellipse=0;Nellipse<99;Nellipse++){
+	// Draw circles at focus points
+	for(int i = 0;i<Nellipse;i++)ellipse[i]->Draw();
 	
-		int xbin, ybin, zbin;
-		tmp->GetMaximumBin(xbin,ybin,zbin);
-		float x = xaxis->GetBinCenter(xbin);
-		float y = yaxis->GetBinCenter(ybin);
-
-		cout<<__FILE__<<":"<<__LINE__<<"x="<<x<<"  y="<<y<<endl;
-		cout<<__FILE__<<":"<<__LINE__<<" frac="<<tmp->GetMaximum()/max<<endl;
-		if(tmp->GetMaximum()/max < 0.25)break;
-
-		int masksize = 15;
-		ellipse[Nellipse] = new TEllipse(x,y,masksize);
-		ellipse[Nellipse]->SetLineColor(Nellipse<3 ? kRed:kBlue);
-		ellipse[Nellipse]->Draw();
-		
-		// Zero out neighbors of max so we can find new max
-		// on next iteration
-		for(int ii=xbin-masksize;ii<=xbin+masksize;ii++){
-			if(ii<1 || ii>NbinsX)continue;
-			for(int jj=ybin-masksize;jj<=ybin+masksize;jj++){
-				if(jj<1 || jj>NbinsY)continue;
-				float r2 = pow((double)ii-xbin,2.0) + pow((double)jj-ybin,2.0);
-				if(sqrt(r2)>masksize)continue;
-				tmp->SetBinContent(ii,jj,0);
-			}
-		}
-		
-	}
-	
+	// Update canvas
 	maincanvas->Update();
-	delete tmp;
 	cout<<endl<<"Done"<<endl;
 	
 	return NOERROR;
 }
 
 //------------------------------------------------------------------
-// FillDensityBin
+// FillDensityHistogram
 //------------------------------------------------------------------
-derror_t MyProcessor::FillDensityBin(float X, float Y)
+derror_t MyProcessor::FillDensityHistogram(TH2F *hist)
 {
-	// Get MCCheatHits and loop over them, adding them to the DQuickFit object
-	DContainer *mccheathits = event_loop->Get("MCCheatHits");
-	MCCheatHit_t *mccheathit = (MCCheatHit_t*)mccheathits->first();
-	for(int i=0;i<mccheathits->nrows;i++, mccheathit++){
-	
-		float x = mccheathit->r*cos(mccheathit->phi);
-		float y = mccheathit->r*sin(mccheathit->phi);
-
-		// decide on using x as a function of y or y as func. of x
-		// so we avoid infinite slope
-		float x1,y1;
-		if(y>x){
-			float m = -x/y;
-			float b = -1.0/(2.0*y)*(x*x + y*y);
-			
-			x1 = (X-m*(b-Y))/(1.0+m*m);
-			y1 = m*x1 + b;
-			
-		}else{
-			//continue;
-			float m = -y/x;
-			float b = -1.0/(2.0*x)*(x*x + y*y);
-			
-			y1 = (Y-m*(b-X))/(1.0+m*m);
-			x1 = m*y1 + b;
-		}
+	/// Loop over all trkhits and fill the density histo for
+	/// any that don't have their used flag set.
+	hist->Reset();
+	DTrkHit **t = trkhit;
+	for(int i=0;i<Ntrkhits;i++ ,t++){
+		if((*t)->used)continue;
 		
-		float d2 = pow((double)(X-x1),2.0) + pow((double)(Y-y1),2.0);
-		float w = exp(-d2*d2/1.0);
-		density->Fill(X,Y,w);
-		//break;
+		(*t)->FillDensityHistogram(hist);
 	}
-
+	
 	return NOERROR;
 }
 
