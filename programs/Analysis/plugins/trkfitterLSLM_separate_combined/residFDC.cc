4cd99eced0cf21b46fe74d07eb30022bcfccd95a
Mark Ito <marki@jlab.org>
2009-04-05 15:37:11 +0000

    compiles

---
+++ programs/Analysis/plugins/trkfitterLSLM_separate_combined/residFDC.cc
@@ -0,0 +1,101 @@
+#include <CLHEP/Matrix/Matrix.h>
+#include <CLHEP/Matrix/Vector.h>
+
+using namespace std;
+
+#include "FDC/DFDCPseudo_factory.h"
+#include "MyTrajectory.h"
+#include "HDGEOMETRY/DLorentzDeflections.h"
+#include "residFDC.h"
+
+const double velDrift = 55.e-4; // cm/ns
+const double c = 29.9792548; // speed of light, cm/ns
+
+// constructor, takes pointer to vector of pseudopoints and a pointer
+// to a trajectory
+residFDC::residFDC(vector<const DFDCPseudo*> *pseudopoints,
+		   MyTrajectory *trajectory,
+		   const DLorentzDeflections *lorentz_def_in, int level) : 
+  n_fdc(pseudopoints->size()), ppPtr(pseudopoints),
+  trajPtr(trajectory), delta(trajPtr->getDelta()),
+  debug_level(level), lorentz_def(lorentz_def_in) {}
+
+void residFDC::calcResids(const vector<double> params) {
+  double docaThis, errorThis, residThis;
+  // do a swim with the input parameters
+  trajPtr->swim(params);
+  HepVector point(3);
+  HepLorentzVector pocaThis;
+  const DFDCPseudo* ppointPtr;
+  for (unsigned int i = 0; i < n_fdc; i++) {
+    ppointPtr = (*ppPtr)[i];
+    point = pseudo2HepVector(*ppointPtr);
+    docaThis = trajPtr->doca(point, pocaThis);
+    errorThis = ERROR_FDC;
+    residThis = residThis/errorThis;
+  }
+}
+
+HepVector residFDC::pseudo2HepVector(const DFDCPseudo &ppoint) {
+  double x;
+  double y;
+  double ct;
+  double z = ppoint.wire->origin(2);
+  trajPtr->getXYT(z, x, y, ct); // on trajectory
+  double delta_x = 0.0, delta_y = 0.0;
+  getCorrectionValue(ppoint, x, y, z, ct, delta_x, delta_y);
+  bool ispos = getCorrectionSign(ppoint, x, y, delta_x, delta_y);
+  HepVector point(3);
+  if (ispos) {
+    point(1) = ppoint.x + delta_x;
+    point(2) = ppoint.y + delta_y;
+  } else {
+    point(1) = ppoint.x - delta_x;
+    point(2) = ppoint.y - delta_y;
+  }
+  point(3) = z;
+  return point;
+}
+
+bool residFDC::getCorrectionSign(const DFDCPseudo &ppoint, double x, double y, double delta_x, double delta_y) {
+  double xWire = ppoint.wire->udir(0);
+  double yWire = ppoint.wire->udir(1);
+  double wireCrossTraj = xWire*(y - ppoint.y) - yWire*(x - ppoint.x);
+  double wireCrossDelta = xWire*delta_y - yWire*delta_x;
+  bool isposTraj = wireCrossTraj > 0?true:false;
+  bool isposDelta = wireCrossDelta > 0?true:false;
+  bool ispos = !(isposTraj ^ isposDelta);
+  if (debug_level > 3) cout << " ppx = " << ppoint.x
+			    << " ppy = " << ppoint.y
+			    << " dx = " << x - ppoint.x
+			    << " dy = " << y - ppoint.y
+			    << " delta_x = " << delta_x
+			    << " delta_y = " << delta_y
+			    << " xWire = " << xWire
+			    << " yWire = " << yWire
+			    << " wireCrossTraj = " << wireCrossTraj
+			    << " wireCrossDelta = " << wireCrossDelta
+			    << " isposTraj = " << isposTraj
+			    << " isposDelta = " << isposDelta
+			    << " ispos = " << ispos
+			    << endl;
+  return ispos;
+}
+
+void residFDC::getCorrectionValue(const DFDCPseudo &ppoint, double x, double y, double z, double ct, double &delta_x, double &delta_y) {
+  double driftDist = (ppoint.time - ct/c)*DRIFT_VELOCITY;
+  //double driftDist = ppoint.time*DRIFT_VELOCITY;
+  double cosangle = ppoint.wire->udir(1);
+  double sinangle= ppoint.wire->udir(0);
+  double alpha = 0.0; // for now
+  if (debug_level > 3) cout << "x, y, z, ct = " << x << ' ' << y << ' ' << z << ' ' << ct << " time, ct/c = " << ppoint.time << " " << ct/c << " driftdist = " << driftDist << endl;
+  double lorentzShift = lorentz_def->GetLorentzCorrection(x, y, z, alpha, driftDist);
+  double ds = -lorentzShift;
+  double dw = driftDist;
+  if (debug_level > 3) cout << "ds, dw" << ds << " " << dw << endl;
+  delta_x = dw*cosangle + ds*sinangle;
+  delta_y = -dw*sinangle + ds*cosangle;
+  return;
+}
+
+// end of C++ source
---
+++ programs/Analysis/plugins/trkfitterLSLM_separate_combined/residFDC.h
@@ -0,0 +1,35 @@
+#ifndef _RESIDFDC_H_
+#define _RESIDFDC_H_
+
+#define PIOVER2 1.570796327
+#define PIOVER4 0.785398163
+#include "FDC/DFDCPseudo.h"
+#include "CDC/DCDCTrackHit.h"
+#include "MyTrajectory.h"
+
+#define BIG_DOUBLE 1.0e12
+#define DRIFT_VELOCITY 55e-4
+#define ERROR_FDC 0.0250
+
+class residFDC {
+ public:
+  residFDC(vector<const DFDCPseudo*> *pseudopoints, MyTrajectory *trajectory,
+		    const DLorentzDeflections *lorentz_def, int level = 1);
+  void calcResids(const vector<double> params);
+ private:
+  unsigned int n_fdc;
+  vector<const DFDCPseudo*> *ppPtr;
+  MyTrajectory *trajPtr;
+  HepVector pseudo2HepVector(const DFDCPseudo &pseudopoint);
+  vector<double> delta;
+  int debug_level;
+  bool getCorrectionSign(const DFDCPseudo &pseudopoint, double x, double y,
+			 double deltaX, double deltaY);
+  void getCorrectionValue(const DFDCPseudo &pseudopoint, double x, double y,
+			  double z, double t, double &delta_x, double &delta_y);
+  const DLorentzDeflections *lorentz_def;
+};
+
+#endif // _RESIDFDC_H_
+
+// end of C++ source
