c7002ebf436677e624e6d3f2eb02760dc1546036
Mark Ito <marki@jlab.org>
2009-04-05 15:19:18 +0000

    calculates residuals for the CDC alone

---
+++ programs/Analysis/plugins/trkfitterLSLM_separate_combined/residCDC.cc
@@ -0,0 +1,86 @@
+// residCDC: class to calculate drift distance, distance of closest
+// approach, point of closest approach, residual and error on residual
+// for the CDC for a given set of track parameters
+
+#include "residCDC.h"
+
+using namespace std;
+
+const double velDrift = 55.e-4; // cm/ns
+const double c = 29.9792548; // speed of light, cm/ns
+
+// constructor, takes pointer to vector of trackhits and a pointer
+// to a trajectory
+residCDC::residCDC(vector<const DCDCTrackHit*> *trackHits,
+				     MyTrajectory *trajectory, int level) : 
+  n_cdc(trackHits->size()), trkhitVectorPtr(trackHits), trajPtr(trajectory), delta(trajPtr->getDelta()),
+  debug_level(level)
+{}
+
+void residCDC::calcResids(const vector<double> params){
+//   params: vector of fit parameters
+  // do a swim with the input parameters
+  trajPtr->swim(params);
+  const DCDCTrackHit* trkhitPtr;
+  DLine line;
+  double docaThis, distThis, residThis, errorThis;
+  HepLorentzVector pocaThis;
+  doca.clear();
+  dist.clear();
+  poca.clear();
+  for (unsigned int j = 0; j < n_cdc; j++) {
+    trkhitPtr = (*trkhitVectorPtr)[j];
+    line = trackhit2line(*trkhitPtr);
+    docaThis = trajPtr->doca(line, pocaThis);
+    distThis = velDrift*(trkhitPtr->tdrift - pocaThis.getT()/c);
+    if (docaThis > distThis) {
+      residThis = (distThis - docaThis)/ERROR_CDC;
+    } else {
+      residThis = innerResidFrac*(distThis - docaThis)/ERROR_CDC;
+    }
+    if (debug_level > 2) cout << "residCDC: j = " << j << " dist = " << distThis << " doca = " << docaThis << " poca xyzt = " << pocaThis.getX() << ' ' << pocaThis.getY() << ' ' << pocaThis.getZ() << ' ' << pocaThis.getT()/c << " resid = " << residThis << endl;
+    errorThis = ERROR_CDC;
+    doca.push_back(docaThis);
+    dist.push_back(distThis);
+    poca.push_back(pocaThis);
+    resid.push_back(residThis);
+    error.push_back(errorThis);
+  }
+  trajPtr->clear();
+};
+
+void residCDC::calcResids(const HepVector* paramsHepPtr) {
+  vector<double> params;
+  for (int i = 0; i < paramsHepPtr->num_row(); i++) {
+    params.push_back((*paramsHepPtr)(i + 1));
+  }
+  calcResids(params);
+  return;
+}
+
+DLine residCDC::trackhit2line(const DCDCTrackHit &trkhit) {
+  const DCDCWire *wire = trkhit.wire;
+  double x = wire->origin.X();
+  double y = wire->origin.Y();
+  double z = wire->origin.Z();
+  //  double phi_wire = atan2(y, x);
+  //  double phi_naive = phi_wire + PIOVER2;
+  //  double theta_naive = wire->stereo;
+  double theta = acos(wire->udir.z());
+  double phi = atan2(wire->udir.y(), wire->udir.x());
+  /*
+  cout << "theta " << theta_naive << " " << theta << " phi " << phi_naive
+       << " "<< phi << endl;
+  */
+  DLine line(x, y, z, theta, phi);
+  return line;
+}
+
+void residCDC::setInnerResidFrac(double innerResidFracIn) {
+  innerResidFrac = innerResidFracIn;
+}
+
+void residCDC::getResids(vector<double> &residRef) {
+  residRef = resid;
+  return;
+}
