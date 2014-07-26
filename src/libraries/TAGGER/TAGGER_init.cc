29955dcc1ef5811a2c82c90ad544ceef8b7bb95a
Richard Jones <jonesrt@jlab.org>
2014-07-26 14:34:02 +0000

    * TAGGER_init.cc [rtj]    - Replaced definitions of obsolete DTagger jana object and associated      factory with DTAGMHit and DTAGFHit jana objects and factories.    - Added declarations of the DTAGMGeometry and DTAGFGeometry jana objects      and associated factories.

--- src/libraries/TAGGER/TAGGER_init.cc
+++ src/libraries/TAGGER/TAGGER_init.cc
@@ -3,14 +3,24 @@
 #include <JANA/JEventLoop.h>
 using namespace jana;
 
-#include "DTagger.h"
-typedef JFactory<DTagger> DTagger_factory;
+#include "DTAGMHit.h"
+#include "DTAGFHit.h"
+#include "DTAGMGeometry.h"
+#include "DTAGFGeometry.h"
+
+typedef JFactory<DTAGMHit> DTAGMHit_factory;
+typedef JFactory<DTAGFHit> DTAGFHit_factory;
+typedef JFactory<DTAGMGeometry> DTAGMGeometry_factory;
+typedef JFactory<DTAGFGeometry> DTAGFGeometry_factory;
 
 
 jerror_t TAGGER_init(JEventLoop *loop)
 {
   /// Create and register TAGGER data factories
-  loop->AddFactory(new DTagger_factory());
+  loop->AddFactory(new DTAGMHit_factory());
+  loop->AddFactory(new DTAGFHit_factory());
+  loop->AddFactory(new DTAGMGeometry_factory());
+  loop->AddFactory(new DTAGFGeometry_factory());
   
   return NOERROR;
 }
