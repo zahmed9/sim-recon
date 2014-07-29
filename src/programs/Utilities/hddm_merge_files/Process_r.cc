f9c56684687fc3daeacb70ee9d9af2e01895e291
Richard Jones <jonesrt@jlab.org>
2014-07-29 15:17:51 +0000

    * hddm_merge_files.cc, Process_s.cc [rtj]    - Converted from c API to c++ API of the HDDM library * Process_r.cc [rtj]    - This one was already using the c++ API of the HDDM library, but it      had a try...catch block around the part of the code that reads from      the file, and was treating all errors as if they were end-of-files.      The HDDM library has been fixed so that it no longer throws an      exception when it reads the last event in an input file, so I removed      this try...catch block to allow real i/o exceptions to be reported.

--- src/programs/Utilities/hddm_merge_files/Process_r.cc
+++ src/programs/Utilities/hddm_merge_files/Process_r.cc
@@ -8,57 +8,51 @@
 using namespace hddm_r;
 
 
-// NOTE: The REST format files claim they are in a compressed format that
-// the C API cannot handle so we use the C++ API here
-
-
 //-----------
 // Process_r  --  HDDM REST format
 //-----------
 void Process_r(unsigned int &NEvents, unsigned int &NEvents_read)
 {
-	// Output file
-	cout<<" output file: "<<OUTFILENAME<<endl;
-	ofstream ofs(OUTFILENAME);
-	hddm_r::ostream ostr(ofs);
+   // Output file
+   std::cout << " output file: " << OUTFILENAME << std::endl;
+   std::ofstream ofs(OUTFILENAME);
+   if (! ofs.is_open()) {
+      std::cout << " Error opening output file \"" << OUTFILENAME 
+                << "\"!" << std::endl;
+      exit(-1);
+   }
+   hddm_r::ostream *ostr = new hddm_r::ostream(ofs);
 
-	if(!ofs.is_open()){
-		cout<<" Error opening output file \""<<OUTFILENAME<<"\"!"<<endl;
-		exit(-1);
-	}
+   // Loop over input files
+   time_t last_time = time(NULL);
+   for (unsigned int i=0; i<INFILENAMES.size(); i++) {
+      std::cout << " input file: " << INFILENAMES[i] << std::endl;
 
-	// Loop over input files
-	time_t last_time = time(NULL);
-	for(unsigned int i=0; i<INFILENAMES.size(); i++){
-		cout<<" input file: "<<INFILENAMES[i]<<endl;
+      // Open hddm file for reading
+      std::ifstream ifs(INFILENAMES[i]);
 
-		// Open hddm file for reading
-		ifstream ifs(INFILENAMES[i]);
+      // Associate input file stream with HDDM record
+      hddm_r::istream *istr = new hddm_r::istream(ifs);
+      
+      // Loop over events
+      while (ifs.good()) {
+         HDDM xrec;
+         *istr >> xrec;
+         NEvents_read++;
 
-		// Associate input file stream with HDDM record
-		hddm_r::istream istr(ifs);
-		
-		// Loop over events
-		while(!ifs.eof() && ifs.good()){
-			try{
-				HDDM xrec;
-				istr >> xrec;
-				NEvents_read++;
-				
-				ostr << xrec;
-				NEvents++;
-			
-				// Update ticker
-				time_t now = time(NULL);
-				if(now != last_time){
-					cout<<"  "<<NEvents_read<<" events read     ("<<NEvents<<" event written) \r";cout.flush();
-					last_time = now;
-				}
-				
-				if(QUIT)break;
-			}catch(...){
-				break;
-			}
-		}
-	}
+         *ostr << xrec;
+         NEvents++;
+
+         // Update ticker
+         time_t now = time(NULL);
+         if (now != last_time) {
+            std::cout << "  " << NEvents_read << " events read     ("
+                      << NEvents << " event written) \r";
+            std::cout.flush();
+            last_time = now;
+         }
+         if (QUIT)
+            break;
+      }
+   }
 }
