cf9d172835746deb34dc3454ffc5b1090a6c5b9b
Richard Jones <jonesrt@jlab.org>
2014-07-31 11:02:07 +0000

    * MyProcessor.h, MyProcessor.cc [rtj]    - Added command line parameter HDDM:USE_INTEGRITY_CHECKS to control whether      automatic data integrity checks are embedded in the output HDDM stream      or not, default is true. The only reason to not include them would be      if one needed to read the stream using the c API, which cannot handle      them nor on-the-fly compression (see parameter HDDM:USE_COMPRESSION).

--- src/programs/Simulation/mcsmear/MyProcessor.cc
+++ src/programs/Simulation/mcsmear/MyProcessor.cc
@@ -59,6 +59,37 @@ jerror_t MyProcessor::init(void)
    fout = new hddm_s::ostream(*ofs);
    Nevents_written = 0;
 
+   HDDM_USE_COMPRESSION = true;
+   gPARMS->SetDefaultParameter("HDDM:USE_COMPRESSION", HDDM_USE_COMPRESSION,
+                          "Turn on/off compression of the output HDDM stream."
+                          " Set to \"0\" to turn off (it's on by default)");
+   HDDM_USE_INTEGRITY_CHECKS = true;
+   gPARMS->SetDefaultParameter("HDDM:USE_INTEGRITY_CHECKS",
+                                HDDM_USE_INTEGRITY_CHECKS,
+                          "Turn on/off automatic integrity checking on the"
+                          " output HDDM stream."
+                          " Set to \"0\" to turn off (it's on by default)");
+
+   // enable on-the-fly bzip2 compression on output stream
+   if (HDDM_USE_COMPRESSION) {
+      jout << " Enabling bz2 compression of output HDDM file stream" 
+           << std::endl;
+      fout->setCompression(hddm_s::k_bz2_compression);
+   }
+   else {
+      jout << " HDDM compression disabled" << std::endl;
+   }
+
+   // enable a CRC data integrity check at the end of each event record
+   if (HDDM_USE_INTEGRITY_CHECKS) {
+      jout << " Enabling CRC data integrity check in output HDDM file stream"
+           << std::endl;
+      fout->setIntegrityChecks(hddm_s::k_crc32_integrity);
+   }
+   else {
+      jout << " HDDM integrity checks disabled" << std::endl;
+   }
+
    // We set the mutex type to "ERRORCHECK" so that if the
    // signal handler is called, we can unlock the mutex
    // safely whether we have it locked or not.
