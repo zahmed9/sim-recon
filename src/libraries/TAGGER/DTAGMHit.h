9613592a78a09d42133019314940762da170856e
Richard Jones <jonesrt@jlab.org>
2014-08-04 23:30:40 +0000

    * DTAGMHit.h [rtj]    - Extended to include a representation of the pulse height (pixels)      and time returned by the fadc; original hit contained only tdc      information and column number encoded as tagger channel energy. * DTAGMDigiHit.h, DTAGMTDCDigiHit.h [rtj]    - New hit types to contain the raw fadc and tdc information coming      from the tagger microscope readout crates. * DTAGHDigiHit.h, DTAGHTDCDigiHit.h [rtj]    - ditto, for the tagger hodoscope. * DTAGMHit_factory.h, DTAGMHit_factory.cc [rtj]    - Factory classes that apply the calibration constants to raw data from      the tagger microscope fadc's and tdc's, and generate normalized hits. * DTAGHHit_factory.h, DTAGHHit_factory.cc [rtj]    - ditto, for the tagger hodoscope.

--- src/libraries/TAGGER/DTAGMHit.h
+++ src/libraries/TAGGER/DTAGMHit.h
@@ -12,21 +12,24 @@
 #include <JANA/JFactory.h>
 
 class DTAGMHit:public jana::JObject{
-	public:
-		JOBJECT_PUBLIC(DTAGMHit);
+   public:
+      JOBJECT_PUBLIC(DTAGMHit);
 
-		double E;
-		double t;
-		int row;
-		int column;
+      double E;
+      double t;
+      int row;
+      int column;
+      double time_fadc;
+      double npix_fadc;
 
-		void toStrings(vector<pair<string,string> > &items)const{
-		  AddString(items, "row", "%d", row);
-		  AddString(items, "column", "%d", column);
-		  AddString(items, "E(GeV)", "%f", E);
-		  AddString(items, "t(ns)", "%f", t);
-		}
+      void toStrings(vector<pair<string,string> > &items) const {
+        AddString(items, "row", "%d", row);
+        AddString(items, "column", "%d", column);
+        AddString(items, "E(GeV)", "%f", E);
+        AddString(items, "t(ns)", "%f", t);
+        AddString(items, "time_fadc(ns)", "%f", E);
+        AddString(items, "npix_fadc", "%f", t);
+      }
 };
 
 #endif // _DTAGMHit_
-
