4c1b376976e11fc769ff77d66b46f3d1874c8e9f
Richard Jones <jonesrt@jlab.org>
2014-08-04 23:32:31 +0000

    * DTAGTDCDigiHit.h [rtj]    - Added a new class to contain the raw tdc hit information      from the tagger hodoscope frontend modules. * TAGGER_init.cc [rtj]    - Add lines to declare all of the new factories introduced in this      revision of the TAGGER library.

--- src/libraries/TAGGER/TAGGER_init.cc
+++ src/libraries/TAGGER/TAGGER_init.cc
@@ -3,23 +3,35 @@
 #include <JANA/JEventLoop.h>
 using namespace jana;
 
+#include "DTAGMDigiHit.h"
+#include "DTAGMTDCDigiHit.h"
+#include "DTAGHDigiHit.h"
+#include "DTAGHTDCDigiHit.h"
 #include "DTAGMHit.h"
-#include "DTAGFHit.h"
+#include "DTAGHHit.h"
 #include "DTAGMGeometry.h"
-#include "DTAGFGeometry.h"
+#include "DTAGHGeometry.h"
 #include "DTAGMGeometry_factory.h"
-#include "DTAGFGeometry_factory.h"
+#include "DTAGHGeometry_factory.h"
+#include "DTAGMHit_factory.h"
+#include "DTAGHHit_factory.h"
 
 
 jerror_t TAGGER_init(JEventLoop *loop)
 {
   /// Create and register TAGGER data factories
-  loop->AddFactory(new JFactory<DTAGMHit>());
-  loop->AddFactory(new JFactory<DTAGFHit>());
+  loop->AddFactory(new JFactory<DTAGMDigiHit>());
+  loop->AddFactory(new JFactory<DTAGMTDCDigiHit>());
+  loop->AddFactory(new JFactory<DTAGHDigiHit>());
+  loop->AddFactory(new JFactory<DTAGHTDCDigiHit>());
+  loop->AddFactory(new DTAGMHit_factory());
+  loop->AddFactory(new DTAGHHit_factory());
   loop->AddFactory(new JFactory<DTAGMHit>("TRUTH"));
-  loop->AddFactory(new JFactory<DTAGFHit>("TRUTH"));
+  loop->AddFactory(new JFactory<DTAGHHit>("TRUTH"));
   loop->AddFactory(new DTAGMGeometry_factory());
-  loop->AddFactory(new DTAGFGeometry_factory());
+  loop->AddFactory(new DTAGHGeometry_factory());
+  loop->AddFactory(new DTAGMGeometry_factory("mc"));
+  loop->AddFactory(new DTAGHGeometry_factory("mc"));
   
   return NOERROR;
 }
