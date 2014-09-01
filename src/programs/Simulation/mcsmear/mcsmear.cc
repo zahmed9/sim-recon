e4670a0227476217f459c124e2e7bbc37221a64e
Richard Jones <jonesrt@jlab.org>
2014-09-01 23:07:07 +0000

    * mcsmear.cc, smear.cc [rtj]    - Rename parameter TAGM_NPE_PER_GEV to TAGM_NPIX_PER_GEV to be more      clear about what it means.

--- src/programs/Simulation/mcsmear/mcsmear.cc
+++ src/programs/Simulation/mcsmear/mcsmear.cc
@@ -89,11 +89,11 @@ bool BCAL_DEBUG_HISTS = false;
 double FTOF_BAR_THRESHOLD    = 0.0;
 double STC_PADDLE_THRESHOLD  = 0.0;
 
-double TAGM_TSIGMA = 0.200;
-double TAGH_TSIGMA = 0.350;
-double TAGM_FADC_TSIGMA = 0.350;
-double TAGH_FADC_TSIGMA = 0.450;
-double TAGM_NPE_PER_GEV = 1.e5;
+double TAGM_TSIGMA = 0.200;        // ns
+double TAGH_TSIGMA = 0.350;        // ns
+double TAGM_FADC_TSIGMA = 0.350;   // ns
+double TAGH_FADC_TSIGMA = 0.450;   // ns
+double TAGM_NPIX_PER_GEV = 1.e5;
 double TAGH_NPE_PER_GEV = 5.e5;
 
 double FCAL_PHOT_STAT_COEF   = 0.0; //0.035;
--- src/programs/Simulation/mcsmear/smear.cc
+++ src/programs/Simulation/mcsmear/smear.cc
@@ -150,7 +150,7 @@ extern double TAGH_TSIGMA;
 // Tagging counter fADC responses
 extern double TAGM_FADC_TSIGMA;
 extern double TAGH_FADC_TSIGMA;
-extern double TAGM_NPE_PER_GEV;
+extern double TAGM_NPIX_PER_GEV;
 extern double TAGH_NPE_PER_GEV;
 
 // Photon-statistics factor for smearing hit energy (from Criss's MC)
@@ -833,7 +833,7 @@ void SmearTAGM(hddm_s::HDDM *record)
          // smear the time
          double t = titer->getT() + SampleGaussian(TAGM_TSIGMA);
          double tADC = titer->getT() + SampleGaussian(TAGM_FADC_TSIGMA);
-         double npe = SamplePoisson(titer->getDE() * TAGM_NPE_PER_GEV);
+         double npe = SamplePoisson(titer->getDE() * TAGM_NPIX_PER_GEV);
          hddm_s::TaggerHitList hits = iter->addTaggerHits();
          hits().setT(t);
          hits().setTADC(tADC);
